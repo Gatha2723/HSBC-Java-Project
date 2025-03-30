2027c4affc7acb601d09f2679fd09e87f8ad0c67
Gatha2723 <ranirebhankar32@gmail.com>
2025-03-30 19:33:00 +0530

    added classfile and MathFunction files

---
+++ src/variables/MathFunction.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class MathFunction {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.max(4,2));
+		System.out.println(Math.max(23456, 98765));
+		System.out.println(Math.min(6, 3));
+		System.out.println(Math.min(3.55888, 6.88888));
+		System.out.println(Math.sqrt(4));
+		System.out.println(Math.abs(-5));
+		System.out.println(Math.random());
+		System.out.println(Math.round(34.51));
+
+	}
+
+}
